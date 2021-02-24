97d7b557008367ceed177300b5618d86a77c2c5f
The Podz <stefanpodz@yahoo.com>
2021-02-24 12:47:10 -0500

    updated html with time blocks and added function in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
-var today = moment();
-$("#currentDay").text(today.format('MMMM Do YYYY, h:mm:ss a'));
+
+$(document).ready(function () {
+    // This code displays the current date and time
+    var today = moment();
+    $("#currentDay").text(today.format('MMMM Do YYYY, h:mm:ss a'));
+
+    // This function allows text to be saved to the text area and local storage
+    $('.saveBtn').on('click', function () {
+        var taskValue = $(".saveBtn").siblings('.description').val();
+        var taskTime = $('.hour').text();
+        console.log(taskValue, taskTime);
+        localStorage.setItem(taskTime, taskValue);
+    })
+    $('#9AM').val(localStorage.getItem('9AM'));
+
+})
+
+
+
+
