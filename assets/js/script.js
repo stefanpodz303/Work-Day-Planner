4dfe521b98445975e3d13d15ee016a923009e32b
The Podz <stefanpodz@yahoo.com>
2021-02-24 18:04:36 -0500

    fixed local storage display issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,21 @@ $(document).ready(function () {
     // This code displays the current date and time
     var today = moment();
     $("#currentDay").text(today.format('MMMM Do YYYY, h:mm:ss a'));
+// //     function update() {
+// //     $('#currentDay’).html(today.format(‘dddd, MMMM Do YYYY, h:mm:ss a’))
+// // }
+//     setInterval(update,1000);
 
     // This function allows text to be saved to the text area and local storage
     $('.saveBtn').on('click', function () {
+        var taskTime = $(this).siblings('.hour').text();
         var taskValue = $(".saveBtn").siblings('.description').val();
-        var taskTime = $('.hour').text();
+        
         console.log(taskValue, taskTime);
         localStorage.setItem(taskTime, taskValue);
     })
     $('#9AM').val(localStorage.getItem('9AM'));
-
+    $('#10AM').val(localStorage.getItem('10AM'));
 })
 
 
