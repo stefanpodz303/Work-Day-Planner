7279ab38fce13131773bb11d3ba558517bcc171c
The Podz <stefanpodz@yahoo.com>
2021-02-25 14:04:53 -0500

    added more local storage features and added save button icons to style the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,21 @@ $(document).ready(function () {
     // This function allows text to be saved to the text area and local storage
     $('.saveBtn').on('click', function () {
         var taskTime = $(this).siblings('.hour').text();
-        var taskValue = $(".saveBtn").siblings('.description').val();
+        var taskValue = $(this).siblings('.description').val();
         
         console.log(taskValue, taskTime);
         localStorage.setItem(taskTime, taskValue);
     })
+    // This allows items in local storage to be displayed in the text area
     $('#9AM').val(localStorage.getItem('9AM'));
     $('#10AM').val(localStorage.getItem('10AM'));
+    $('#11AM').val(localStorage.getItem('11AM'));
+    $('#12PM').val(localStorage.getItem('12PM'));
+    $('#1PM').val(localStorage.getItem('1PM'));
+    $('#2PM').val(localStorage.getItem('2PM'));
+    $('#3PM').val(localStorage.getItem('3PM'));
+    $('#4PM').val(localStorage.getItem('4PM'));
+    $('#5PM').val(localStorage.getItem('5PM'));
 })
 
 
